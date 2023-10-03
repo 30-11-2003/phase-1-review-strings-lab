cc243404fad2af64b47d9d80dcae2fe6e0f57a7e
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-03 16:03:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!;
+const excitedwelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome , ${currentUser.slice(0,1)} !;
